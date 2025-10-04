adc6ba14d76ef34c87387aaff1b433e7016e9b45
NicolasCardinaux <nicocardina01@gmail.com>
2025-10-04 13:40:03 -0300

    cambios front

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import '../styles/Footer.css';
+import '../styles/footer.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { 
   faMapMarkerAlt, 
